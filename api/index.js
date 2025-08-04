e686bb6209bfc3432184cbac1113fe7141fce25a
akankshasalokhe <akankshasalokhe2001@gmail.com>
2025-08-04 12:56:14 +0530

    update

--- api/index.js
+++ api/index.js
@@ -31,9 +31,9 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
 module.exports = app;
 module.exports.handler = serverless(app);
